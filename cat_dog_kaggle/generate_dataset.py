d68d59092b010df250b07b4f0c8d7e6a9db2fd00
Akbar Gumbira <akbargumbira@gmail.com>
2017-05-12 07:44:42 +0200

    Less memory hungry code.

--- cat_dog_kaggle/generate_dataset.py
+++ cat_dog_kaggle/generate_dataset.py
@@ -1,33 +1,33 @@
 # coding=utf-8
 import os
 import fnmatch
+import getpass
 import numpy as np
 from keras.preprocessing import image as keras_image
 
 
 def generate_dataset(image_dir):
     # Training data
-    id, training_data, training_labels = [], [], []
+    id, training_labels = np.empty((0)), np.empty((0))
+    training_data = np.empty(shape=[0, 224, 224, 3])
     for root, dirnames, filenames in os.walk(image_dir):
         filenames = fnmatch.filter(filenames, '*.[Jj][Pp][Gg]')
         for index, filename in enumerate(filenames):
             image_path = os.path.join(root, filename)
             if os.path.exists(image_path):
-                id.append(filename)
+                id = np.append(id, filename)
                 image = keras_image.load_img(image_path, target_size=(224, 224))
-                image_x = keras_image.img_to_array(image)
-                image_x = np.expand_dims(image_x, axis=0)
-                training_data.extend(image_x)
+                image = keras_image.img_to_array(image)
+                image = np.expand_dims(image, axis=0)
+                training_data = np.vstack((training_data, image))
                 if 'dog' in filename.lower():
-                    training_labels.append(1)
+                    training_labels = np.append(training_labels, 1)
                 else:
-                    training_labels.append(0)
-    id_clean = np.char.replace(id, '.jpg', '')
-    np.savez_compressed(open('model/training_id.npz', 'w'), np.array(id_clean))
-    np.savez_compressed(open('model/training_data.npz', 'w'), np.array(
-        training_data))
-    np.savez_compressed(open('model/training_label.npz', 'w'), np.array(
-        training_labels))
+                    training_labels = np.append(training_labels, 0)
+    id = np.char.replace(id, '.jpg', '')
+    np.savez_compressed(open('model/training_id.npz', 'w'), id)
+    np.savez_compressed(open('model/training_data.npz', 'w'), training_data)
+    np.savez_compressed(open('model/training_label.npz', 'w'), training_labels)
 
-image_dir = '/home/agumbira/dev/data/dog_cat_kaggle/train'
+image_dir = '/home/%s/dev/data/dog_cat_kaggle/train' % getpass.getuser()
 generate_dataset(image_dir)
